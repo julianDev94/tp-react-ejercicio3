3dff7a54e40b3f8cf836c264c7ef3c9aa929928d
JulianAballay <juliancabj1210@gmail.com>
2024-01-30 21:48:21 -0300

    Agrego el evento onclick al boton para modificar el state y mostrar el msj

--- src/components/Mensaje.jsx
+++ src/components/Mensaje.jsx
@@ -1,15 +1,26 @@
-import React from 'react';
-import { Button } from 'react-bootstrap';
+import React from "react";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 
-const Mensaje = ({valorMensaje}) => {
-    return (
-        <section>
-            <article>
-            <p>Hello {valorMensaje}</p>
-            </article>
-            <Button></Button>
-        </section>
-    );
+const Mensaje = ({ valorMensaje }) => {
+  const [msj, setMsj] = useState("");
+
+  const mostrarMsj = () => {
+    const valorMensaje2 = "(from changed state)";
+    setMsj(valorMensaje2);
+  };
+
+  return (
+    <section>
+      <p className="fs-3 fw-bold">
+        Hello {valorMensaje} {msj}
+      </p>
+
+      <Button variant="secondary" onClick={mostrarMsj}>
+        Click me
+      </Button>
+    </section>
+  );
 };
 
 export default Mensaje;
